2cb80a26db24febe4e2c66ec3555f7506c98dd96
rafiki80 <82238823+rafiki80@users.noreply.github.com>
2022-04-15 00:52:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 from art import banner
+import os
 
-print(banner)
+
+def clear_console():
+    os.system('clear')
 
 
-def first_calc():
-    resultado = (userinput2 * userinput1) / 100
-    print("El", userinput2, "tanto por ciento de", userinput1, "es:", resultado)
+def first_calc(a, b):
+    resultado = (a * b) / 100
+    print(f"El {b} por ciento de {a} es: {resultado}")
     return resultado
 
 
-userinput1 = float(input("Escribe el total del que quieras sacar un tanto por ciento: \n"))
-userinput2 = float(input("Tanto por ciento a calcular:\n"))
+def new_operation():
+    fin2 = False
+    while not fin2:
+        userinput4 = input("""Quieres hacer alguna operación más? \nContesta S o N: """)
+        userinput4 = userinput4.lower()
+        if userinput4 == "n":
+            clear_console()
+            print(banner)
+            print("Adiós!! ;-)")
+            fin2 = True
+            return True
+        elif userinput4 == "s":
+            fin2 = True
+            clear_console()
+
 
 fin = False
 while not fin:
-    fin2 = False
-    per_cent = first_calc()
+    print(banner)
+    userinput1 = float(input("Escribe el total del que quieras sacar un tanto por ciento: \n"))
+    userinput2 = float(input("Tanto por ciento a calcular:\n"))
+
+    per_cent = first_calc(userinput1, userinput2)
     userinput3 = input("¿Sumar o restar al total? Escribe + o -: ")
     if userinput3 == "+":
         print("La suma es:", userinput1 + per_cent)
         print("Operación terminada")
-        while not fin2:
-            userinput4 = input("""Quieres hacer alguna operación más? \nContesta S o N: """)
-            if userinput4 == "n":
-                print("Adiós!! ;-)")
-                fin2 = True
-                fin = True
-            elif userinput4 == 's':
-                fin2 = True
+        fin = new_operation()
 
     elif userinput3 == "-":
         print("La resta es:", userinput1 - per_cent)
         print("Operación terminada")
-        while not fin2:
-            userinput4 = input("""Quieres hacer alguna operación más? \nContesta S o N: """)
-            if userinput4 == "n":
-                print("Adiós!! ;-)")
-                fin2 = True
-                fin = True
-            elif userinput4 == 's':
-                fin2 = True
+        fin = new_operation()
+
     else:
         print("Operación terminada")
-        userinput4 = input("""Quieres hacer alguna operación más? \nContesta S o N: """)
-        if userinput4 == "n":
-            print("Adiós!! ;-)")
-            fin = True
+        fin = new_operation()
